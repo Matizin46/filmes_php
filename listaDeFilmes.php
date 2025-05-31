bc9da106b46edca56d39b0a573d1fcc83106e20c
Matizin46 <mattheus.paes.br@gmail.com>
2025-05-30 22:18:23 -0300

    Fatiar

---
+++ listaDeFilmes.php
@@ -0,0 +1,70 @@
+<?php
+include "cabecalho.php"
+?>
+<h2>Todos os filmes</h2>
+
+<div class="lista-filmes">
+    <div class="card-filme">
+        <img src="img/filme1.webp">
+        <h3>Jurassic Park</h3>
+        <span>⭐ 8/10</span>
+    </div>
+    <div class="card-filme">
+        <img src="img/filme2.jpg">
+        <h3>Karate Kid Legends</h3>
+        <span>⭐ 9/10</span>
+    </div>
+    <div class="card-filme">
+        <img src="img/Lilo.webp">
+        <h3>Lilo e Stitch</h3>
+        <span>⭐ 10/10</span>
+    </div>
+    <div class="card-filme">
+        <img src="img/Pecadores.jpg">
+        <h3>Pecadores</h3>
+        <span>⭐ 7/10</span>
+    </div>
+    <div class="card-filme">
+        <img src="img/filme1.webp">
+        <h3>Jurassic Park</h3>
+        <span>⭐ 10/10</span>
+    </div>
+    <div class="card-filme">
+        <img src="img/filme2.jpg">
+        <h3>Karate Kid Legends</h3>
+        <span>⭐ 10/10</span>
+    </div>
+    <div class="card-filme">
+        <img src="img/Lilo.webp">
+        <h3>Lilo e Stitch</h3>
+        <span>⭐ 10/10</span>
+    </div>
+    <div class="card-filme">
+        <img src="img/Pecadores.jpg">
+        <h3>Pecadores</h3>
+        <span>⭐ 10/10</span>
+    </div>
+    <div class="card-filme">
+        <img src="img/filme1.webp">
+        <h3>Jurassic Park</h3>
+        <span>⭐ 10/10</span>
+    </div>
+    <div class="card-filme">
+        <img src="img/filme2.jpg">
+        <h3>Karate Kid Legends</h3>
+        <span>⭐ 10/10</span>
+    </div>
+    <div class="card-filme">
+        <img src="img/Lilo.webp">
+        <h3>Lilo e Stitch</h3>
+        <span>⭐ 10/10</span>
+    </div>
+    <div class="card-filme">
+        <img src="img/Pecadores.jpg">
+        <h3>Pecadores</h3>
+        <span>⭐ 10/10</span>
+    </div>
+</div>
+<?php
+include "rodape.php"
+?>
---
+++ rodape.php
@@ -0,0 +1,6 @@
+<footer class="rodape">
+<img src="img/logo.png">
+<small><?php echo date("Y"); ?></small>
+</footer>
+</body>
+</html>
